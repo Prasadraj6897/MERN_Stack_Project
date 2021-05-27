c3d4981e1d9e6f572b7ab980936824d242c71f78
Prasadraj6897 <prasadraj32895@gmail.com>
2021-05-27 18:37:02 +0530

    Created Babel Packages

--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ import UserRouter from './Routers/UserRouter/UserRouter.js'
 import ImageRouter from './Routers/UploadRouter/UploadRouter.js'
 
 const app = express();
-
+// app.use(express.json())
+app.use(cookieParser())
 dotenv.config();
 
 //for passing data from client to server we need bodyparser
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 
-app.use(cookieParser())
+
 app.use(fileUpload({
   useTempFiles: true
 }))
@@ -29,7 +30,8 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Accept-Language, Content-Language, Authorization, Cookie, Host");
+    res.header("Access-Control-Allow-Credentials",true);
     next();
   });
 
